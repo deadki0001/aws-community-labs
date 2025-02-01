3b56b9540b51504c8c05ab91e4797427ece17fe3
Devon Adkins <adkins.devon@hotmail.com>
2025-02-01 15:39:05 +0200

    adding redirect to AWS

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template, request, jsonify, redirect, url_fo
 from app.models import User, Challenge, Score
 from app import db
 import os
+import requests
 import secrets
 from app.email_utils import EmailService
 from datetime import datetime
